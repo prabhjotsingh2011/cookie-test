a861228f676a59b5cbf7b1349253337ba7495e9f
prabhjot <psingh20112002@gmail.com>
2023-07-03 19:05:59 +0530

    initial

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(cookieParser());
+
+// Set the cookie
+app.get('/set-cookie', (req, res) => {
+  res.cookie('myCookie', 'cookieValue');
+  res.send('Cookie set!');
+});
+
+// Fetch the cookie
+app.get('/fetch-cookie', (req, res) => {
+  const myCookie = req.cookies.myCookie;
+  res.send(`Cookie value: ${myCookie}`);
+});
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log('Server listening on port 4000');
+});
